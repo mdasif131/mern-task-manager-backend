084826f3b08faa60bc5faf145a1b303f314cafe0
mdasif131 <asifmdasifchy@gmail.com>
2026-01-03 20:31:51 +0600

    set headers

--- src/middlewares/AuthVerifyMiddleware.js
+++ src/middlewares/AuthVerifyMiddleware.js
@@ -4,8 +4,15 @@ import asycHandler from './asyncHandler.js';
 export const authenticate = asycHandler(async (req, res, next) => {
   let token;
   //Read token from cookies
-  token = req.cookies?.token 
- 
+  token = req.cookies?.token;
+
+  // If no token in cookies, check Authorization header
+  if (!token && req.headers.authorization) {
+    if (req.headers.authorization.startsWith('Bearer ')) {
+      token = req.headers.authorization.split(' ')[1];
+    }
+  }
+
   if (token) {
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
@@ -13,7 +20,6 @@ export const authenticate = asycHandler(async (req, res, next) => {
       next();
     } catch (error) {
       res.status(401).json({ message: 'Not authorized, no token' });
-
     }
   } else {
     res.status(401).json({ message: 'Not authorized, no token' });
